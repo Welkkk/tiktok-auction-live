f81cc90b59d6304ba5be2b7e5e38f7958e9071ee
Welkkk <viclbr3@gmail.com>
2026-05-25 18:45:38 +0200

    Update vite.config.js

--- tiktok-ranking-app/vite.config.js
+++ tiktok-ranking-app/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "tiktok-auction-live.onrender.com"
+    ]
+  },
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "tiktok-auction-live.onrender.com"
+    ]
+  }
+});
